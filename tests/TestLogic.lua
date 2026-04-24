336e1cd22420c2b2d5f4c8fd1089b6efdfb286f7
maybe-adamant <maybe.adamant@gmail.com>
2026-04-24 17:44:20 -0400

    adding test coverage

---
+++ tests/TestLogic.lua
@@ -0,0 +1,197 @@
+local lu = require("luaunit")
+
+TestBiomeControlLogic = {}
+
+function TestBiomeControlLogic:testPatchPlanAppliesAndRevertsRoomAndNpcMutations()
+    local harness = ResetBiomeControlHarness({
+        config = {
+            ModeStoryArachne = 2,
+            PackedStoryArachneMin = 5,
+            PackedStoryArachneMax = 7,
+            ModeMiniBossTreant = 1,
+            NPCSpacing = 9,
+            PreventEchoScam = true,
+        },
+    })
+
+    local okApply, applyErr = lib.lifecycle.applyMutation(harness.internal.definition, harness.store)
+    lu.assertTrue(okApply, tostring(applyErr))
+
+    lu.assertEquals(RoomData.F_Story01.ForceAtBiomeDepthMin, 5)
+    lu.assertEquals(RoomData.F_Story01.ForceAtBiomeDepthMax, 7)
+    lu.assertEquals(RoomData.F_Story01.GameStateRequirements[1].Value, 5)
+    lu.assertEquals(RoomData.F_Story01.GameStateRequirements[2].Value, 7)
+    lu.assertEquals(#RoomData.F_MiniBoss01.GameStateRequirements, 1)
+    lu.assertEquals(RoomData.F_MiniBoss01.GameStateRequirements[1].Value, -1)
+    lu.assertEquals(NamedRequirementsData.NoRecentFieldNPCEncounter[1].SumPrevRooms, 9)
+    lu.assertEquals(#RoomData.H_MiniBoss01.GameStateRequirements, 1)
+    lu.assertEquals(RoomData.H_MiniBoss01.GameStateRequirements[1].Value, 3)
+
+    local okRevert, revertErr = lib.lifecycle.revertMutation(harness.internal.definition, harness.store)
+    lu.assertTrue(okRevert, tostring(revertErr))
+
+    lu.assertNil(RoomData.F_Story01.ForceAtBiomeDepthMin)
+    lu.assertNil(RoomData.F_Story01.ForceAtBiomeDepthMax)
+    lu.assertEquals(RoomData.F_Story01.GameStateRequirements[1].Value, 4)
+    lu.assertEquals(RoomData.F_Story01.GameStateRequirements[2].Value, 8)
+    lu.assertEquals(#RoomData.F_MiniBoss01.GameStateRequirements, 0)
+    lu.assertEquals(NamedRequirementsData.NoRecentFieldNPCEncounter[1].SumPrevRooms, 6)
+    lu.assertEquals(#RoomData.H_MiniBoss01.GameStateRequirements, 0)
+end
+
+function TestBiomeControlLogic:testBiomePriorityFiltersEligibleLootUntilSatisfied()
+    ResetBiomeControlHarness({
+        registerHooks = true,
+        config = {
+            Enabled = true,
+            PrioritizeSpecificRewardEnabled = true,
+            PriorityBiome1 = "ApolloUpgrade",
+        },
+        CurrentRun = {
+            ClearedBiomes = 0,
+        },
+        GetEligibleLootNames = function()
+            return { "ZeusUpgrade", "ApolloUpgrade" }
+        end,
+    })
+
+    lu.assertEquals(GetEligibleLootNames({}), { "ApolloUpgrade" })
+
+    GiveLoot({ ForceLootName = "ApolloUpgrade" })
+    lu.assertEquals(GetEligibleLootNames({}), { "ZeusUpgrade", "ApolloUpgrade" })
+end
+
+function TestBiomeControlLogic:testTrialRewardPrioritySetsEncounterLootPair()
+    ResetBiomeControlHarness({
+        registerHooks = true,
+        config = {
+            Enabled = true,
+            PrioritizeTrialRewardEnabled = true,
+            PriorityTrial1 = "ApolloUpgrade",
+            PriorityTrial2 = "ZeusUpgrade",
+        },
+        CurrentRun = {},
+        GetInteractedGodsThisRun = function()
+            return { "ApolloUpgrade", "ZeusUpgrade" }
+        end,
+        GetEligibleLootNames = function(excluded)
+            if excluded and excluded[1] == "ApolloUpgrade" then
+                return { "ZeusUpgrade" }
+            end
+            return { "ApolloUpgrade", "ZeusUpgrade" }
+        end,
+    })
+
+    local room = {
+        ChosenRewardType = "Devotion",
+        Encounter = {},
+    }
+    SetupRoomReward(CurrentRun, room, nil, {})
+
+    lu.assertEquals(room.Encounter.LootAName, "ApolloUpgrade")
+    lu.assertEquals(room.Encounter.LootBName, "ZeusUpgrade")
+end
+
+function TestBiomeControlLogic:testForcedNpcEncounterNarrowsLegalEncounterList()
+    ResetBiomeControlHarness({
+        registerHooks = true,
+        config = {
+            Enabled = true,
+            ModeNPCArtemisErebus = 2,
+            PackedNPCArtemisErebusMin = 4,
+            PackedNPCArtemisErebusMax = 10,
+        },
+        CurrentRun = {},
+        ChooseEncounter = function(_, _, args)
+            return args.LegalEncounters
+        end,
+    })
+
+    local currentRun = {
+        BiomeDepthCache = 4,
+    }
+    local room = {
+        RoomSetName = "F",
+        LegalEncounters = {
+            "NemesisCombatF",
+            "ArtemisCombatF",
+        },
+    }
+    local result = ChooseEncounter(currentRun, room, {})
+
+    lu.assertEquals(result, { "ArtemisCombatF" })
+end
+
+function TestBiomeControlLogic:testOnlyAllowForcedEncountersFiltersUnforcedNpcEncounters()
+    ResetBiomeControlHarness({
+        registerHooks = true,
+        config = {
+            Enabled = true,
+            OnlyAllowForcedEncounters = true,
+        },
+        CurrentRun = {},
+        ChooseEncounter = function(_, _, args)
+            return args.LegalEncounters
+        end,
+    })
+
+    local currentRun = {
+        BiomeDepthCache = 4,
+    }
+    local room = {
+        RoomSetName = "F",
+        LegalEncounters = {
+            "NemesisCombatF",
+            "ArtemisCombatF",
+            "GenericCombatF",
+        },
+    }
+    local result = ChooseEncounter(currentRun, room, {})
+
+    lu.assertEquals(result, { "GenericCombatF" })
+end
+
+function TestBiomeControlLogic:testFieldsTwoRewardHookOverridesEarlyCombatRooms()
+    ResetBiomeControlHarness({
+        registerHooks = true,
+        config = {
+            Enabled = true,
+            ForceTwoRewardFieldsOpeners = true,
+        },
+    })
+
+    local result = SelectFieldsDoorCageCount({
+        BiomeDepthCache = 2,
+    }, {
+        Name = "H_Combat01",
+        MinDoorCageRewards = 2,
+    })
+
+    lu.assertEquals(result, 2)
+end
+
+function TestBiomeControlLogic:testDreamRouteSetsNextRoomSetAndPool()
+    ResetBiomeControlHarness({
+        registerHooks = true,
+        config = {
+            Enabled = true,
+            DreamRouteEnabled = true,
+            DreamRouteBiome1 = "G",
+            DreamRouteBiome2 = "I",
+            DreamRouteBiome3 = "N",
+            DreamRouteBiome4 = "P",
+        },
+        CurrentRun = {
+            IsDreamRun = true,
+            EnteredBiomes = 0,
+            CurrentRoom = {},
+        },
+        GameState = {},
+    })
+
+    SelectNextDreamBiome(nil)
+
+    lu.assertEquals(CurrentRun.CurrentRoom.NextRoomSet, { "G" })
+    lu.assertEquals(CurrentRun.DreamBiomePool, { "I", "N", "P" })
+    lu.assertEquals(GameState.LastDreamStartingBiome, "G")
+end
---
+++ tests/TestUtils.lua
@@ -0,0 +1,227 @@
+public = {}
+_PLUGIN = { guid = "test-biome-control" }
+
+local function deepCopy(orig)
+    if type(orig) ~= "table" then
+        return orig
+    end
+    local copy = {}
+    for key, value in pairs(orig) do
+        copy[key] = deepCopy(value)
+    end
+    return copy
+end
+
+rom = {
+    mods = {},
+    game = {
+        DeepCopyTable = deepCopy,
+        SetupRunData = function() end,
+    },
+    ImGui = {},
+    ImGuiCol = {
+        Text = 1,
+    },
+    gui = {
+        add_to_menu_bar = function() end,
+        add_imgui = function() end,
+    },
+}
+
+rom.mods["SGG_Modding-ENVY"] = {
+    auto = function()
+        return {}
+    end,
+}
+
+rom.mods["SGG_Modding-Chalk"] = {
+    auto = function()
+        return { DebugMode = false }
+    end,
+    original = function(config)
+        return config
+    end,
+}
+
+local registeredWraps = {}
+
+modutil = {
+    once_loaded = {
+        game = function() end,
+    },
+    mod = {
+        Path = {
+            Wrap = function(path, handler)
+                registeredWraps[path] = handler
+                local base = _G[path]
+                _G[path] = function(...)
+                    return handler(base, ...)
+                end
+            end,
+        },
+    },
+}
+rom.mods["SGG_Modding-ModUtil"] = modutil
+
+local function color()
+    return { 255, 255, 255, 255 }
+end
+
+game = {
+    Color = {
+        AphroditeVoice = color(),
+        ApolloVoice = color(),
+        AresVoice = color(),
+        DemeterVoice = color(),
+        HephaestusVoice = color(),
+        HeraDamage = color(),
+        HestiaVoice = color(),
+        PoseidonVoice = color(),
+        ZeusVoice = color(),
+    },
+}
+
+local function contains(list, value)
+    for _, current in ipairs(list or {}) do
+        if current == value then
+            return true
+        end
+    end
+    return false
+end
+
+import = function(path, fenv, ...)
+    local libPath = "../../adamant-ModpackLib/src/" .. path
+    local modulePath = "src/" .. path
+    local chunk = loadfile(libPath, "t", fenv or _ENV) or loadfile(modulePath, "t", fenv or _ENV)
+    return assert(chunk, "unable to import " .. tostring(path))(...)
+end
+
+dofile("../../adamant-ModpackLib/src/main.lua")
+lib = public
+rom.mods["adamant-ModpackLib"] = lib
+
+local function installBaseGlobals(opts)
+    opts = opts or {}
+
+    CurrentRun = opts.CurrentRun
+    GameState = opts.GameState or {}
+    RoomData = deepCopy(opts.RoomData or {
+        F_Story01 = {
+            Name = "F_Story01",
+            GameStateRequirements = {
+                { Path = { "CurrentRun", "BiomeDepthCache" }, Comparison = ">=", Value = 4 },
+                { Path = { "CurrentRun", "BiomeDepthCache" }, Comparison = "<=", Value = 8 },
+            },
+        },
+        F_MiniBoss01 = {
+            Name = "F_MiniBoss01",
+            GameStateRequirements = {},
+        },
+        H_MiniBoss01 = {
+            Name = "H_MiniBoss01",
+            GameStateRequirements = {},
+        },
+        H_MiniBoss02 = {
+            Name = "H_MiniBoss02",
+            GameStateRequirements = {},
+        },
+    })
+    RoomSetData = deepCopy(opts.RoomSetData or {
+        F = {
+            F_Combat05 = { Name = "F_Combat05" },
+        },
+    })
+    NamedRequirementsData = deepCopy(opts.NamedRequirementsData or {
+        NoRecentFieldNPCEncounter = {
+            { SumPrevRooms = 6 },
+        },
+    })
+    EncounterData = deepCopy(opts.EncounterData or {
+        ArtemisCombatF = {},
+        ArtemisCombatG = {},
+        NemesisCombatF = {},
+    })
+
+    Contains = contains
+    GetEligibleLootNames = opts.GetEligibleLootNames or function()
+        return { "ZeusUpgrade", "ApolloUpgrade" }
+    end
+    GiveLoot = opts.GiveLoot or function(args)
+        return args
+    end
+    SetupRoomReward = opts.SetupRoomReward or function() end
+    ChooseEncounter = opts.ChooseEncounter or function(_, _, args)
+        return args
+    end
+    SelectNextDreamBiome = opts.SelectNextDreamBiome or function()
+        return "base"
+    end
+    SelectFieldsDoorCageCount = opts.SelectFieldsDoorCageCount or function()
+        return 3
+    end
+    GetInteractedGodsThisRun = opts.GetInteractedGodsThisRun or function()
+        return {}
+    end
+    IsGameStateEligible = opts.IsGameStateEligible or function()
+        return true
+    end
+    IsEncounterEligible = opts.IsEncounterEligible or function()
+        return true
+    end
+end
+
+local function applyOverrides(target, overrides)
+    for key, value in pairs(overrides or {}) do
+        target[key] = value
+    end
+end
+
+function ResetBiomeControlHarness(opts)
+    opts = opts or {}
+    registeredWraps = {}
+    installBaseGlobals(opts)
+
+    RunDirectorBiomeControl_Internal = {
+        definition = {
+            modpack = "run-director",
+            id = "BiomeControl",
+            name = "Biome Control",
+            default = false,
+            affectsRunData = true,
+        },
+        DEFAULT_FIELD_MEDIUM = 0.4,
+        REGION_UNDERWORLD = 1,
+        REGION_SURFACE = 2,
+    }
+
+    local internal = RunDirectorBiomeControl_Internal
+    dofile("src/mods/data.lua")
+    dofile("src/mods/logic.lua")
+    internal.BuildDefinitionStorage()
+
+    local dataDefaults = dofile("src/config.lua")
+    local config = deepCopy(dataDefaults)
+    applyOverrides(config, opts.config)
+
+    local store, session = lib.createStore(config, internal.definition, dataDefaults)
+    internal.store = store
+
+    if opts.registerHooks then
+        internal.host = lib.createModuleHost({
+            definition = internal.definition,
+            store = store,
+            session = session,
+            hookOwner = internal,
+            registerHooks = internal.RegisterHooks,
+        })
+    end
+
+    return {
+        internal = internal,
+        config = config,
+        store = store,
+        session = session,
+        wrappers = registeredWraps,
+    }
+end
